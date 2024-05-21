5cf4eb6a35a58fe86abb6ef3216a9c31039a7a78
Alex Leoniuk <alex.brest.by@gmail.com>
2024-05-21 21:10:37 +0300

    feat: base routing add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
 import classes from './app.module.css'
 
-function App() {
-
+const App: React.FC = () => {
   return (
     <div className={classes.wrapper}>
-
+      <Navigation />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './components/App/App';
+import Home from './pages/Home';
+import Archive from './pages/Archive';
+import Blog from './pages/Blog';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="archive" element={<Archive />} />
+          <Route path="blog" element={<Blog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
